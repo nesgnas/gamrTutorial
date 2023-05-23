21dd2f5e958370762852a9102e68d9bf90125350
nguyenhuybao03 <nguyenhuybao82@icloud.com>
2023-05-23 13:18:35 +0700

    credit state update

--- src/main/java/main/UI1.java
+++ src/main/java/main/UI1.java
@@ -1,4 +1,5 @@
 package main;
+
 import Tiles.UtilityTool;
 import entity.Player;
 
@@ -13,8 +14,8 @@ import java.io.IOException;
 import static main.gamePanel.soundEffect;
 import static main.gamePanel.soundMusic;
 
-public class UI1 implements MouseListener{
-    //main.keyHandle keyHandle = new keyHandle(this); // call keyHandle.class
+public class UI1 implements MouseListener {
+    // main.keyHandle keyHandle = new keyHandle(this); // call keyHandle.class
 
     gamePanel gp;
     public BufferedImage menu, cont, reset, sound_music_on, sound_music_off,
@@ -23,47 +24,85 @@ public class UI1 implements MouseListener{
     public static int commandNum_music = 1;
     public static int commandNum_effect = 3;
     public int subState = 0;
-    public boolean messageOn =false;
+    public boolean messageOn = false;
     public String message = "";
     int messageCounter = 0;
     public String currentDialogue = "";
     Font arial_30, arial_80B;
     Graphics2D g2;
     MouseEvent e;
+
     public UI1(gamePanel gp) {
         this.gp = gp;
         getImage();
-        arial_30 = new Font("ThaleahFat",Font.BOLD,30);
-        arial_80B = new Font("ThaleahFat",Font.PLAIN,80);
-        //setupFonts();
+        arial_30 = new Font("ThaleahFat", Font.BOLD, 30);
+        arial_80B = new Font("ThaleahFat", Font.PLAIN, 80);
+        // setupFonts();
     }
-    public void showMessage(String text){
+
+    public void showMessage(String text) {
         message = text;
         messageOn = true;
     }
+
     public void draw(Graphics2D g2) {
         this.g2 = g2;
         g2.setFont(arial_30);
         g2.setColor(Color.white);
-        draw_pause(70,105);
-        g2.drawString("Room: " + Player.getRoomPlayerIn(), 45,115);
-        //Start game - Title state
-        if (gp.gameState == gp.titleState){
+        draw_pause(70, 105);
+        g2.drawString("Room: " + Player.getRoomPlayerIn(), 45, 115);
+        // Start game - Title state
+        if (gp.gameState == gp.titleState) {
             drawTitleScreen();
         }
-        //Play State
-        if (gp.gameState == gp.playState){
-            //Do playState stuff later
+        if (gp.gameState == gp.creditState) {
+            drawCreditState();
         }
-        //Pause State
-        if (gp.gameState == gp.optionsState){
+        // Play State
+        if (gp.gameState == gp.playState) {
+            // Do playState stuff later
+        }
+        // Pause State
+        if (gp.gameState == gp.optionsState) {
             drawOptionsScreen();
         }
-        //Dialogue State
-        if (gp.gameState == gp.dialogue){
+        // Dialogue State
+        if (gp.gameState == gp.dialogue) {
             drawDialogueScreen();
         }
     }
+
+    private void drawCreditState() {
+        // BACKGOURND
+        g2.drawImage(bg, 0, 0, gp.worldWidth, gp.worldHeight, null);
+        // Name
+        // BAO
+        String text = "NGUYEN HUY BAO ITDSIU21076";
+        int x = getXForCenteredText(text);
+        int y = gp.getTitleSize() * 3;
+        g2.setColor(Color.BLACK);
+        g2.drawString(text, x - 100, y);
+        // NTNT
+        String text1 = "NGUYEN TRAN NGUYEN ANH ITITWE20021";
+
+        g2.setColor(Color.BLACK);
+        g2.drawString(text1, x - 100, y + 100);
+        // My
+        String text2 = "NGUYEN HUYNH THAO MY ITCSIU21204";
+
+        g2.setColor(Color.BLACK);
+        g2.drawString(text2, x - 100, y + 200);
+        // THU HOANG itcsiu21063
+        String text3 = "LE THU HOANG ITCSIU21063  ";
+        g2.setColor(Color.BLACK);
+        g2.drawString(text3, x - 100, y + 300);
+        // Sang
+        String text4 = "BUI DOAN THE SANG ITCSIU21104 ";
+        g2.setColor(Color.BLACK);
+        g2.drawString(text4, x - 100, y + 400);
+
+    }
+
     void drawTitleScreen() {
 
         // BACKGOURND
@@ -94,133 +133,145 @@ public class UI1 implements MouseListener{
                 null);
 
     }
-    public void drawSubWindow(int x, int y, int width, int height){
-        //mousePressed(e);
-        //mouseClicked(e);
+
+    public void drawSubWindow(int x, int y, int width, int height) {
+        // mousePressed(e);
+        // mouseClicked(e);
         Color color = new Color(0, 0, 0, 186);
         g2.setColor(color);
-        g2.fillRoundRect(x,y,width,height,35,35);
+        g2.fillRoundRect(x, y, width, height, 35, 35);
 
-        color = new Color(255,255, 255);
+        color = new Color(255, 255, 255);
         g2.setColor(color);
         g2.setStroke(new BasicStroke(5));
-        g2.drawRoundRect(x+5,y+5,width-10,height-10,25,25);
+        g2.drawRoundRect(x + 5, y + 5, width - 10, height - 10, 25, 25);
     }
-    public void drawDialogueScreen(){
-        //Window
+
+    public void drawDialogueScreen() {
+        // Window
         int x = gp.getTitleSize() * 2;
         int y = gp.getTitleSize() / 2;
-        int width = gp.getScreenWidth() - (gp.getTitleSize()*4);
+        int width = gp.getScreenWidth() - (gp.getTitleSize() * 4);
         int height = gp.getTitleSize() * 4;
-        drawSubWindow(x,y,width,height);
+        drawSubWindow(x, y, width, height);
 
-        g2.setFont(g2.getFont().deriveFont(Font.PLAIN,32F));
+        g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 32F));
         x += gp.getTitleSize();
         y += gp.getTitleSize();
-        for (String line : currentDialogue.split("\n")){
-            g2.drawString(line,x,y);
+        for (String line : currentDialogue.split("\n")) {
+            g2.drawString(line, x, y);
             y += 40;
         }
-        g2.drawString(currentDialogue,x,y);
+        g2.drawString(currentDialogue, x, y);
     }
+
     public void drawOptionsScreen() {
         g2.setColor(Color.white);
-        //g2.setFont(g2.getFont().deriveFont(40F));
+        // g2.setFont(g2.getFont().deriveFont(40F));
         int frameX = (int) (gp.getTitleSize() * 5.5);
         int frameY = gp.getTitleSize() * 4;
         int frameWidth = (int) (gp.getTitleSize() * 9.5);
         int frameHeight = (int) (gp.getTitleSize() * 7.5);
         drawSubWindow(frameX, frameY, frameWidth, frameHeight);
         option_top(frameX, frameY);
-//        if (soundMusic) {
-//            commandNum_music = 1;
-//        }
-//        else {
-//            commandNum_music = 2;
-//        }
-//        if  (soundEffect) {
-//            commandNum_effect = 3;
-//        }
-//        else{
-//            commandNum_effect = 4;
-//        }
+        // if (soundMusic) {
+        // commandNum_music = 1;
+        // }
+        // else {
+        // commandNum_music = 2;
+        // }
+        // if (soundEffect) {
+        // commandNum_effect = 3;
+        // }
+        // else{
+        // commandNum_effect = 4;
+        // }
     }
 
-    public int getXForCenteredText(String text){
-        int length = (int)g2.getFontMetrics().getStringBounds(text,g2).getWidth();
-        int x = gp.getScreenWidth()/2 - length/2;
+    public int getXForCenteredText(String text) {
+        int length = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+        int x = gp.getScreenWidth() / 2 - length / 2;
         return x;
     }
-    public void draw_pause(int textX, int textY){
+
+    public void draw_pause(int textX, int textY) {
         g2.drawImage(pause, textX, textY - 70, gp.getTitleSize(), gp.getTitleSize(), null); // method to draw
-        //System.out.println("textX: " + textX + " - " + "textY" + textY);
+        // System.out.println("textX: " + textX + " - " + "textY" + textY);
     }
-    public void option_top(int frameX, int frameY){
+
+    public void option_top(int frameX, int frameY) {
         int textX;
         int textY;
-        g2.setFont(g2.getFont().deriveFont(Font.BOLD,40F));
+        g2.setFont(g2.getFont().deriveFont(Font.BOLD, 40F));
         int titlesize = gp.getTitleSize() + 10;
-        //Title
+        // Title
         String text = "Settings";
-        textX = getXForCenteredText(text)-10;
+        textX = getXForCenteredText(text) - 10;
         textY = frameY;
-        g2.drawString("Settings", textX+10, textY+gp.getTitleSize());
+        g2.drawString("Settings", textX + 10, textY + gp.getTitleSize());
         g2.setFont(g2.getFont().deriveFont(20F));
 
-        //Reset Room
+        // Reset Room
         textX = frameX + gp.getTitleSize();
-        textY += gp.getTitleSize()*2;
-        g2.drawString("Reset", textX + 70,textY+gp.getTitleSize());
-        g2.drawImage(reset, textX +70, textY+gp.getTitleSize() - 70, titlesize - 10, titlesize - 10, null); // method to draw
-        //System.out.println("textX: " + textX + " - " + "textY" + textY);
-        textY+=gp.getTitleSize();
+        textY += gp.getTitleSize() * 2;
+        g2.drawString("Reset", textX + 70, textY + gp.getTitleSize());
+        g2.drawImage(reset, textX + 70, textY + gp.getTitleSize() - 70, titlesize - 10, titlesize - 10, null); // method
+                                                                                                               // to
+                                                                                                               // draw
+        // System.out.println("textX: " + textX + " - " + "textY" + textY);
+        textY += gp.getTitleSize();
         textX += 70;
 
-        //Home
-        textY += gp.getTitleSize()*2;
-        g2.drawString("Home",textX + 150,textY-gp.getTitleSize()*2);
-        //g2.drawString("Home", textX,textY);
-        g2.drawImage(menu, textX+150, textY-gp.getTitleSize()*2 - 70, titlesize - 10, titlesize - 10, null); // method to draw
-        //System.out.println("textX: " + textX + " - " + "textY" + textY);
+        // Home
+        textY += gp.getTitleSize() * 2;
+        g2.drawString("Home", textX + 150, textY - gp.getTitleSize() * 2);
+        // g2.drawString("Home", textX,textY);
+        g2.drawImage(menu, textX + 150, textY - gp.getTitleSize() * 2 - 70, titlesize - 10, titlesize - 10, null); // method
+                                                                                                                   // to
+                                                                                                                   // draw
+        // System.out.println("textX: " + textX + " - " + "textY" + textY);
         textX += 150;
 
-
-        //Continue
-        //textY += frameY;
+        // Continue
+        // textY += frameY;
         text = "Continue";
         textY += 50;
         textX = getXForCenteredText(text) - 10;
-        g2.drawString("Continue", textX+10,textY -50);
+        g2.drawString("Continue", textX + 10, textY - 50);
         g2.drawImage(cont, textX + 20, textY - 120, titlesize - 10, titlesize - 10, null); // method to draw
-        //System.out.println("textX: " + textX + " - " + "textY" + textY);
-        textY -=50;
+        // System.out.println("textX: " + textX + " - " + "textY" + textY);
+        textY -= 50;
 
         // Music
         text = "Music";
         textX = frameX + gp.getTitleSize();
-        if (commandNum_music == 1){
-            g2.drawString("Music: ON", textX+60,textY + gp.getTitleSize()*2);
-            g2.drawImage(sound_music_on, textX + 70, textY + gp.getTitleSize()*2 - 70, titlesize - 10, titlesize - 10, null); // method to draw
+        if (commandNum_music == 1) {
+            g2.drawString("Music: ON", textX + 60, textY + gp.getTitleSize() * 2);
+            g2.drawImage(sound_music_on, textX + 70, textY + gp.getTitleSize() * 2 - 70, titlesize - 10, titlesize - 10,
+                    null); // method to draw
         }
-        if (commandNum_music == 2){
-            g2.drawString("Music: OFF", textX+60,textY + gp.getTitleSize()*2);
-            g2.drawImage(sound_music_off, textX + 70, textY + gp.getTitleSize()*2 - 70, titlesize - 10, titlesize - 10, null); // method to draw
+        if (commandNum_music == 2) {
+            g2.drawString("Music: OFF", textX + 60, textY + gp.getTitleSize() * 2);
+            g2.drawImage(sound_music_off, textX + 70, textY + gp.getTitleSize() * 2 - 70, titlesize - 10,
+                    titlesize - 10, null); // method to draw
         }
         textX += 70;
-        //System.out.println("textX: " + textX + " - " + "textY" + textY);
+        // System.out.println("textX: " + textX + " - " + "textY" + textY);
 
         // Effect
         if (commandNum_effect == 3) {
-            g2.drawString("Effect: ON", textX+135, textY + gp.getTitleSize()*2);
-            g2.drawImage(sound_effect_on, textX+150, textY + gp.getTitleSize()*2 - 70, titlesize - 10, titlesize - 10, null); // method to draw
+            g2.drawString("Effect: ON", textX + 135, textY + gp.getTitleSize() * 2);
+            g2.drawImage(sound_effect_on, textX + 150, textY + gp.getTitleSize() * 2 - 70, titlesize - 10,
+                    titlesize - 10, null); // method to draw
         }
         if (commandNum_effect == 4) {
-            g2.drawString("Effect: OFF", textX+135, textY + gp.getTitleSize()*2);
-            g2.drawImage(sound_effect_off, textX+150, textY + gp.getTitleSize()*2 - 70, titlesize - 10, titlesize - 10, null); // method to draw
+            g2.drawString("Effect: OFF", textX + 135, textY + gp.getTitleSize() * 2);
+            g2.drawImage(sound_effect_off, textX + 150, textY + gp.getTitleSize() * 2 - 70, titlesize - 10,
+                    titlesize - 10, null); // method to draw
         }
-        textX+= 150;
-        //System.out.println("textX: " + textX + " - " + "textY" + textY);
-        textY += gp.getTitleSize()*2;
+        textX += 150;
+        // System.out.println("textX: " + textX + " - " + "textY" + textY);
+        textY += gp.getTitleSize() * 2;
     }
 
     public void getImage() { // read Image
@@ -239,7 +290,6 @@ public class UI1 implements MouseListener{
             sound = ImageIO.read(new File("data/ui/sound.png"));
             credit = ImageIO.read(new File("data/ui/credit.png"));
 
-
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -249,39 +299,38 @@ public class UI1 implements MouseListener{
     public void mouseClicked(MouseEvent e) {
         int mx = e.getX();
         int my = e.getY();
-        if (mx>=342 && mx<=342+50 && my>= 312 && my<=312+50) { //Reset room
-            //resetRoom = true;
-            //gamePanel.state = "game";
+        if (mx >= 342 && mx <= 342 + 50 && my >= 312 && my <= 312 + 50) { // Reset room
+            // resetRoom = true;
+            // gamePanel.state = "game";
             System.out.println("Reset room");
-        }
-        else
-        if (mx>=577 && mx<=577+50 && my>= 312 && my<=312+50) { //Menu
-            //gamePanel.state = "game";
+        } else if (mx >= 577 && mx <= 577 + 50 && my >= 312 && my <= 312 + 50) { // Menu
+            // gamePanel.state = "game";
             System.out.println("Menu");
-        }
-        else
-        if (mx>=443 && mx<=443+50 && my>= 408 && my<=408+50) { //Continous
-            //gamePanel.state = "game";
+        } else if (mx >= 443 && mx <= 443 + 50 && my >= 408 && my <= 408 + 50) { // Continous
+            // gamePanel.state = "game";
             System.out.println("Continous");
-        }
-        else
-        if (mx>=342 && mx<=342+50 && my>= 504 && my<= 504+50) { //Music
-            //gamePanel.state = "game";
+        } else if (mx >= 342 && mx <= 342 + 50 && my >= 504 && my <= 504 + 50) { // Music
+            // gamePanel.state = "game";
             System.out.println("Sound Music");
-        }
-        else
-        if (mx>=572 && mx<=572+50 && my>= 504 && my<=504+50) { //Effect
-            //gamePanel.state = "game";
+        } else if (mx >= 572 && mx <= 572 + 50 && my >= 504 && my <= 504 + 50) { // Effect
+            // gamePanel.state = "game";
             System.out.println("Sound Effect");
         }
     }
-    @Override
-    public void mousePressed(MouseEvent e) {}
-    @Override
-    public void mouseReleased(MouseEvent e) {}
-    @Override
-    public void mouseEntered(MouseEvent e) {}
-    @Override
-    public void mouseExited(MouseEvent e) {}
-}
 
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+}
